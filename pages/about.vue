f20f2a4ee1bef3208a3edbdbc967dc540d4164c1
sboris <smrnvbrs@gmail.com>
2020-10-30 18:58:54 +0300

    initial, added base admin page structure, added axios and connected it with api to see products

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: 'test'
+            }
+        },
+        methods: {
+            onClick() {
+                alert('i am here')
+            }
+        }
+    }
+</script>
+
+<style>
+
+
+</style>
